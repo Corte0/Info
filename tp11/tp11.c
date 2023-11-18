4e671caf34e3c8eaf50090cbb436123d114b2e75
Corte0 <108155580+Corte0@users.noreply.github.com>
2023-11-18 17:00:33 -0300

    TP11 terminado

--- tp11/tp11.c
+++ tp11/tp11.c
@@ -2,9 +2,9 @@
 
 typedef struct 
 {
-    int id;
+    unsigned long long int id ;
     float precio;
-    char nombre[50];
+    char nombre[150];
 }producto_t;
 
 
@@ -16,6 +16,8 @@ typedef struct
 
 
 void inicializar_producto(producto_t*p);
+void imprimir_producto (producto_t *p);
+void imprimir_producto_en_archivo (producto_t *p,FILE *dst);
 
 
 int main(){
@@ -38,20 +40,49 @@ int main(){
         inicializar_producto(&estadistica.caro);
         inicializar_producto(&aux);
 
-        char string [100];
-        fgets(string,sizeof(string),datos_parseados);
-        sscanf(string,"%d,%f,%[^/]s",&aux.id,&aux.precio,&aux.nombre);
+        char string [250];
 
-        printf("%d, %f, %s",aux.id, aux.precio, aux.nombre );
+        estadistica.barato.precio = 100000;
 
+        while (!feof(datos_parseados)){
+            fgets(string,sizeof(string),datos_parseados);
+            sscanf(string,"%lld,%f,%[^/]s",&aux.id,&aux.precio,aux.nombre);
+            //imprimir_producto(&aux);
+            if (aux.precio < estadistica.barato.precio){
+                estadistica.barato = aux;
+            }else if (aux.precio > estadistica.caro.precio){
+                estadistica.caro = aux;
+            }
+        }
+
+        printf("\nProducto mas barato: \n");
+        imprimir_producto(&estadistica.barato);
+        printf("\nProducto mas caro: \n");
+        imprimir_producto(&estadistica.caro);
+
+        fprintf(datos_estadisticos,"Producto mas barato: \n");
+        imprimir_producto_en_archivo(&estadistica.barato,datos_estadisticos);
+        fprintf(datos_estadisticos,"\nProducto mas caro: \n");
+        imprimir_producto_en_archivo(&estadistica.caro,datos_estadisticos);  
 
         fclose(datos_parseados);
         fclose(datos_estadisticos);
     }
+}
 
+void imprimir_producto (producto_t *p){
+    printf("ID: %lld\n",p->id);
+    printf("Precio: %.2f\n",p->precio);
+    printf("Nombre: %s",p->nombre);
+}
+
+void imprimir_producto_en_archivo (producto_t *p,FILE *dst){
+    fprintf(dst,"ID: %lld\n",p->id);
+    fprintf(dst,"Precio: %.2f\n",p->precio);
+    fprintf(dst,"Nombre: %s",p->nombre);
 }
 
 void inicializar_producto(producto_t*p){
-    p->id=0;
-    p->precio=0;
+    p->id = 0;
+    p->precio = 0;
 }
